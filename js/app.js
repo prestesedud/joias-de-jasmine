e31d6d02d96df32269210d3f9f3a50a6a433a473
prestesedud <davidupts@gmail.com>
2025-12-23 01:00:16 -0300

    primeiro commit

---
+++ js/app.js
@@ -0,0 +1,30 @@
+let swiper = new Swiper(".swiper", {
+  effect: "coverflow",
+  grabCursor: true,
+  centeredSlides: true,
+  initialSlide: 2,
+  speed: 600,
+  slidesPerView: "auto",
+  coverflowEffect: {
+    rotate: 0,
+    stretch: 80,
+    depth: 350,
+    modifier: 1,
+    slideShadows: true,
+  },
+  on: {
+    click(swiper, event) {
+      if (swiper.clickedIndex !== undefined) {
+        swiper.slideTo(swiper.clickedIndex);
+      }
+    },
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  }
+});
